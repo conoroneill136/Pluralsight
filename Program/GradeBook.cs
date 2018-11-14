c56ac5eb39f9766891347d8c5a1fcd4c00dabd93
conoroneill136 <conoroneill136@gmail.com>
2018-11-14 22:51:12 +0000

    branching tutorial done didnt add switching as seemed redundant

--- Program/GradeBook.cs
+++ Program/GradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,6 +33,14 @@ namespace Program
                 }
             }
 
+        }
+
+        public void WriteGrades(TextWriter destination)
+        {
+            for (int i = 0; i < grades.Count; i++)
+            {
+                destination.WriteLine(grades[i]);
+            }
         }
 
         public event NameChangedDelegate NameChanged;
--- Program/GradeStatistics.cs
+++ Program/GradeStatistics.cs
@@ -8,6 +8,35 @@ namespace Program
 {
     public class GradeStatistics
     {
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AverageGrade >= 90)
+                {
+                    result = "A";
+                }
+                else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+                else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+                else if (AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+                else
+                {
+                    result = "f";
+                }
+                return result;
+            }                    
+        }
+
         public GradeStatistics()
         {
             HighestGrade = 0;
